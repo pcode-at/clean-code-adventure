9c6020cd6990646164146c9cbe2a11063c21da3a
npeham <npeham@pcode.at>
2019-01-10 22:48:29 +0100

    add euclideanAlgorithm

---
+++ src/vanilla-js/algorithms/euclideanAlgorithm/euclideanAlgorithm.js
@@ -0,0 +1,3 @@
+export const getGgtUsingEuclideanAlgorithm = (numberOne = 0, numberTwo = 0) => {
+  return 0;
+}
---
+++ src/vanilla-js/algorithms/euclideanAlgorithm/euclideanAlgorithm.test.js
@@ -0,0 +1,19 @@
+import { getGgtUsingEuclideanAlgorithm } from "./euclideanAlgorithm";
+
+describe("euclidean algorithm", () => {
+  it('should return the correct ggt for both numbers - level 1', () => {
+    const numberOne = 100;
+    const numberTwo = 20;
+    expect(getGgtUsingEuclideanAlgorithm(numberOne, numberTwo)).toBe(5);
+  }),
+  it('should return the correct ggt for both numbers - level 2', () => {
+    const numberOne = 6666;
+    const numberTwo = 567;
+    expect(getGgtUsingEuclideanAlgorithm(numberOne, numberTwo)).toBe(3);
+  }),
+  it('should return the correct ggt for both numbers - level 3', () => {
+    const numberOne = 56987589735
+    const numberTwo = 23458;
+    expect(getGgtUsingEuclideanAlgorithm(numberOne, numberTwo)).toBe(1);
+  })
+});
