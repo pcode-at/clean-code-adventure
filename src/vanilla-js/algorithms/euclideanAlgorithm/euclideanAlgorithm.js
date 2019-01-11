64db852e5578ba6d14dd180f885ce442519ae515
npeham <npeham@pcode.at>
2019-01-11 11:59:33 +0100

    add research link

--- src/vanilla-js/algorithms/euclideanAlgorithm/euclideanAlgorithm.js
+++ src/vanilla-js/algorithms/euclideanAlgorithm/euclideanAlgorithm.js
@@ -1,3 +1,4 @@
+// https://www.lernhelfer.de/schuelerlexikon/mathematik/artikel/euklidischer-algorithmus
 export const getGgtUsingEuclideanAlgorithm = (numberOne = 0, numberTwo = 0) => {
   return 0;
 }
