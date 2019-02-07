fd4b79325165bccbdcdfcdb7bd42f0b04871204b
Jonas Dorfinger <36422131+dorfingerjonas@users.noreply.github.com>
2019-02-07 11:52:43 +0100

    made the code more beautiful with adding functions

--- src/vanilla-js/object/object.js
+++ src/vanilla-js/object/object.js
@@ -13,9 +13,34 @@ export const fncs = {
         person.dateOfBirth = new Date(parseInt(date[2]), parseInt(date[1]), parseInt(date[0]) - 1);
         let currentAge = localDate.getFullYear() - person.dateOfBirth.getFullYear();
 
-        if (localDate.getMonth() < person.dateOfBirth.getMonth() || localDate.getMonth() === person.dateOfBirth.getMonth() && localDate.getDay < person.dateOfBirth.getDay || localDate.getDay === person.dateOfBirth.getDay) {
-            currentAge--;
+        if (monthIsLowerThan() || monthIsEqualTo() && dayIsLowerThan() || dayIsEqualTo()) {
+            return currentAge - 1;
         }
-        return currentAge;
+        
+        // return currentAge;
+        
+        function monthIsLowerThan() {
+            if (localDate.getMonth() < person.dateOfBirth.getMonth()) {
+                return true;
+            }
+        }
+        
+        function monthIsEqualTo() {
+            if (localDate.getMonth() === person.dateOfBirth.getMonth()) {
+                return true;
+            }
+        }
+        
+        function dayIsLowerThan() {
+            if (localDate.getDay < person.dateOfBirth.getDay) {
+                return true;
+            }
+        }
+        
+        function dayIsEqualTo() {
+            if (localDate.getDay === person.dateOfBirth.getDay) {
+                return true;
+            }
+        }
     }
 };
