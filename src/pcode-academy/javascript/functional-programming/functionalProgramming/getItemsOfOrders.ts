f2803dd0186ca7fafbfa51eb420cb59257344bc0
Nico Peham <npeham@Nicos-Mac-mini.local>
2019-02-15 17:46:59 +0100

    add whitespace

--- src/pcode-academy/javascript/functional-programming/functionalProgramming/getItemsOfOrders.ts
+++ src/pcode-academy/javascript/functional-programming/functionalProgramming/getItemsOfOrders.ts
@@ -4,9 +4,11 @@ import { Item } from '../model/Item.model';
 
 export const getItemsOfOrdersImperative = (orders: Order[]): Item[] => {
   let items: Item[] = [];
+
   orders.forEach(order => {
     items = [...items, ...order.items];
   });
+
   return items;
 };
 
